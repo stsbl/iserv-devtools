c88ce98a6fc2938728bce8f575e963522621cb6c
Felix Jacobi <felix.jacobi@stsbl.de>
2016-10-18 01:10:44 +0200

    Added link generation script.

---
+++ links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+export LC_ALL=C
+
+ln -s $PWD/scripts/winst/commit /usr/local/sbin/stsbl-dev-winst-commit-files
+ln -s $PWD/scripts/winst/install /usr/local/sbin/stsbl-dev-winst-install-files
+ln -s $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mods-commit-files
+ln -s $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mods-install-files
+ln -s $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
