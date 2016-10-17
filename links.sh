aa43f12f3ba61bea540dfb57c2e3148bbede7f2a
Felix Jacobi <felix.jacobi@stsbl.de>
2016-10-18 01:13:13 +0200

    Fixed typo.

--- links.sh
+++ links.sh
@@ -4,6 +4,6 @@ export LC_ALL=C
 
 ln -s $PWD/scripts/winst/commit /usr/local/sbin/stsbl-dev-winst-commit-files
 ln -s $PWD/scripts/winst/install /usr/local/sbin/stsbl-dev-winst-install-files
-ln -s $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mods-commit-files
-ln -s $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mods-install-files
+ln -s $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mod-commit-files
+ln -s $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
 ln -s $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
