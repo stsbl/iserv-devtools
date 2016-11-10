8044d859adcafe82c05617fa110b0cbddd9e6268
Felix Jacobi <felix.jacobi@stsbl.de>
2016-11-10 06:38:38 +0100

    Added dpkg utils:
    changelog: Create changelogs from git log.
    
    build: Build source package in quilt format to binary.

--- links.sh
+++ links.sh
@@ -2,8 +2,10 @@
 
 export LC_ALL=C
 
-ln -s $PWD/scripts/winst/commit /usr/local/sbin/stsbl-dev-winst-commit-files
-ln -s $PWD/scripts/winst/install /usr/local/sbin/stsbl-dev-winst-install-files
-ln -s $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mod-commit-files
-ln -s $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
-ln -s $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
+ln -sf $PWD/scripts/winst/commit /usr/local/sbin/stsbl-dev-winst-commit-files
+ln -sf $PWD/scripts/winst/install /usr/local/sbin/stsbl-dev-winst-install-files
+ln -sf $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mod-commit-files
+ln -sf $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
+ln -sf $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
+ln -sf $PWD/scripts/dpkg/build /usr/local/sbin/stsbl-dev-dpkg-build
+ln -sf $PWD/scripts/dpkg/changelog /usr/local/sbin/stsbl-dev-dpkg-changelog
