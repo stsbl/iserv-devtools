58a2f82e6c586fdf8904244a990d704152897d4b
Felix Jacobi <felix.jacobi@stsbl.de>
2017-03-19 15:22:54 +0100

    Added gettext/update.

--- links.sh
+++ links.sh
@@ -10,3 +10,4 @@ ln -sf $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
 ln -sf $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
 ln -sf $PWD/scripts/dpkg/build /usr/local/sbin/stsbl-dev-dpkg-build
 ln -sf $PWD/scripts/dpkg/changelog /usr/local/sbin/stsbl-dev-dpkg-changelog
+ln -sf $PWD/scripts/gettext/update /usr/local/bin/stsbl-dev-gettext
