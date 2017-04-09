11b6452c74f139fe71c8bb1be1d2c2396a1eca89
Felix Jacobi <felix.jacobi@stsbl.de>
2017-04-09 15:35:11 +0200

    Added dpkg/make-src.

--- links.sh
+++ links.sh
@@ -11,6 +11,7 @@ ln -sf $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
 ln -sf $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
 ln -sf $PWD/scripts/dpkg/build /usr/local/sbin/stsbl-dev-dpkg-build
 ln -sf $PWD/scripts/dpkg/changelog /usr/local/sbin/stsbl-dev-dpkg-changelog
+ln -sf $PWD/scripts/dpkg/make-src /usr/local/sbin/stsbl-dev-dpkg-make-src
 ln -sf $PWD/scripts/gettext/update /usr/local/bin/stsbl-dev-gettext
 ln -sf $PWD/scripts/gettext/edit-po /usr/local/bin/stsbl-dev-edit-po
 ln -sf $PWD/scripts/gettext/edit-js-po /usr/local/bin/stsbl-dev-edit-js-po
