fa36501820eb992941daab36988b97bb5299d4db
Felix Jacobi <felix.jacobi@stsbl.de>
2017-04-09 15:32:19 +0200

    Replaced winst/newchangelog by new winst/changelog.

--- links.sh
+++ links.sh
@@ -5,7 +5,7 @@ export LC_ALL=C
 ln -sf $PWD/tools/dummy-editor /usr/local/bin/dummy-editor
 ln -sf $PWD/scripts/winst/commit /usr/local/sbin/stsbl-dev-winst-commit-files
 ln -sf $PWD/scripts/winst/install /usr/local/sbin/stsbl-dev-winst-install-files
-ln -sf $PWD/scripts/winst/newchangelog /usr/local/sbin/stsbl-dev-winst-newchangelog
+ln -sf $PWD/scripts/winst/changelog /usr/local/bin/stsbl-dev-winst-changelog
 ln -sf $PWD/scripts/mods/commit /usr/local/sbin/stsbl-dev-mod-commit-files
 ln -sf $PWD/scripts/mods/install /usr/local/sbin/stsbl-dev-mod-install-files
 ln -sf $PWD/scripts/mods/python-wrapper /usr/local/sbin/stsbl-dev-python-helper
