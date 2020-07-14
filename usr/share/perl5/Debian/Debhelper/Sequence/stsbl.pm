136dae0b7501d54db564bcc2b5135d35a67f34b3
Felix Jacobi <Felix@jacobi-hamburg.net>
2020-07-14 22:42:44 +0200

    Bumped copyright

--- usr/share/perl5/Debian/Debhelper/Sequence/stsbl.pm
+++ usr/share/perl5/Debian/Debhelper/Sequence/stsbl.pm
@@ -4,6 +4,7 @@ use strict;
 use Debian::Debhelper::Dh_Lib;
 
 remove_command("dh_iservcopyright");
+remove_command("dh_iservcontrol");
 insert_before("dh_iservinstall3", "dh_iservassetsbuild");
 insert_after("dh_iservinstall3", "dh_iservassetsclean");
 
