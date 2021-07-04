f1a957de8390f27527a027dd1e0b957f8329a98c
Felix Jacobi <Felix@jacobi-hamburg.net>
2021-07-04 21:23:50 +0200

    dh-sequence-stsbl: Updated for compat level 13

--- usr/share/perl5/Debian/Debhelper/Sequence/stsbl.pm
+++ usr/share/perl5/Debian/Debhelper/Sequence/stsbl.pm
@@ -4,8 +4,16 @@ use strict;
 use Debian::Debhelper::Dh_Lib;
 
 remove_command("dh_iservcopyright");
-remove_command("dh_iservcontrol");
-insert_before("dh_iservinstall3", "dh_iservassetsbuild");
-insert_after("dh_iservinstall3", "dh_iservassetsclean");
+
+if (!compat("13")) # if (compat < 13)
+{
+  insert_before("dh_iservinstall3", "dh_iservassetsbuild");
+  insert_after("dh_iservinstall3", "dh_iservassetsclean");
+}
+else
+{
+  insert_before("dh_iservinstall", "dh_iservassetsbuild");
+  insert_after("dh_iservinstall", "dh_iservassetsclean");
+}
 
 1;
